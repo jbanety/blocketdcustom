315bf3631567014b9d20b62e5beb796cf341333a
jbanety <jbanety@gmail.com>
2015-01-05 18:54:22 +0100

    Correction install.sql

--- sql/install.sql
+++ sql/install.sql
@@ -1,14 +1,16 @@
-CREATE TABLE `#__etd_custom` (
+CREATE TABLE IF NOT EXISTS `#__etd_custom` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `hook` varchar(50) NOT NULL,
+  `etdhook` varchar(50) DEFAULT NULL,
   `access` int(10) unsigned NOT NULL DEFAULT '0',
   `showtitle` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `css` varchar(255) NOT NULL,
   `params` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `hook` (`hook`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
-CREATE TABLE `#__etd_custom_lang` (
+CREATE TABLE IF NOT EXISTS `#__etd_custom_lang` (
   `id_custom` int(10) unsigned NOT NULL,
   `id_lang` int(10) unsigned NOT NULL,
   `title` varchar(255) NOT NULL,
@@ -16,10 +18,11 @@ CREATE TABLE `#__etd_custom_lang` (
   PRIMARY KEY (`id_custom`,`id_lang`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `#__etd_custom_shop` (
+CREATE TABLE IF NOT EXISTS `#__etd_custom_shop` (
   `id_custom` int(10) NOT NULL,
   `id_shop` int(10) NOT NULL,
   `published` tinyint(1) NOT NULL DEFAULT '0',
   `ordering` int(10) NOT NULL,
+  `exceptions` text NOT NULL,
   PRIMARY KEY (`id_custom`,`id_shop`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
